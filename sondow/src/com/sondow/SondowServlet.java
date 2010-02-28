a7c79bd8951c319d9419d7fb345ac3e30da4cb9c
joesondow <joesondow@9cedc2b2-b939-11de-82ee-51df2961d3cf>
2010-02-28 03:16:14 +0000

    Changed sample servlet to forward to a JSP
    git-svn-id: https://sondow.googlecode.com/svn/trunk@8 9cedc2b2-b939-11de-82ee-51df2961d3cf

--- sondow/src/com/sondow/SondowServlet.java
+++ sondow/src/com/sondow/SondowServlet.java
@@ -2,6 +2,8 @@ package com.sondow;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,9 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 @SuppressWarnings("serial")
 public class SondowServlet extends HttpServlet {
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		RequestDispatcher dispatcher = request
+				.getRequestDispatcher("/home.jsp");
+		if (dispatcher != null) {
+			dispatcher.forward(request, response);
+		}
 	}
 }
